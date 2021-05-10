2afb45a0018d840f5fc93f989c2749944e90c1c9
Sown <sown@protonmail.com>
2021-05-10 14:27:47 -0700

    Adding validation

--- p3/src/components/pages/AccountPage.vue
+++ p3/src/components/pages/AccountPage.vue
@@ -7,75 +7,76 @@
         </div>
 
         <div v-else id="loginForm">
-            <h2>Login</h2>
-            <small>
-                (Form is prefilled for demonstration purposes; remove in final
-                application)
-            </small>
-            <div>
-                <label>
-                    Email:
-                    <input type="text" v-model="data.email" />
-                </label>
-            </div>
-            <div>
-                <label>
-                    Password:
-                    <input type="password" v-model="data.password" />
-                </label>
-            </div>
+            <div v-if="registerView">
+                <h2>Login</h2>
+                <p>Don't have an account? <a href="#" v-on:click="switchToLoginView">Click here</a> to register.</p>
+                <div>
+                    <label>
+                        Email:
+                        <input type="text" v-model="data.email" v-on:blur="validate"/>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Password:
+                        <input type="password" v-model="data.password" v-on:blur="validate"/>
+                    </label>
+                </div>
 
-            <button v-on:click="login">Login</button>
-
-            <ul v-if="errors">
-                <li class="error" v-for="(error, index) in errors" :key="index">
-                    {{ error }}
-                </li>
-            </ul>
-
-            <h2>Register</h2>
-            <div>
-                <label>
-                    Name:
-                    <input type="text" v-model="data.name" />
-                </label>
+                <button v-on:click="login">Login</button>
+                <ul v-if="errors">
+                    <li class="error" v-for="(error, index) in errors" :key="index">
+                        {{ error }}
+                    </li>
+                </ul>
             </div>
-            <div>
-                <label>
-                    Email:
-                    <input type="text" v-model="data.email" />
-                </label>
-            </div>
-            <div>
-                <label>
-                    Password:
-                    <input type="password" v-model="data.password" />
-                </label>
-            </div>
+            <div v-else>
+                <h2>Register</h2>
+                <p>Already have an account? <a href="#" v-on:click="switchToRegisterView">Click here</a> to log in.</p>
+                <div>
+                    <label>
+                        Name:
+                        <input type="text" v-model="data.name" v-on:blur="validate"/>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Email:
+                        <input type="text" v-model="data.email" v-on:blur="validate"/>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Password:
+                        <input type="password" v-model="data.password" v-on:blur="validate"/>
+                    </label>
+                </div>
 
-            <button v-on:click="register">Register</button>
+                <button v-on:click="register">Register</button>
 
-            <ul v-if="errors">
-                <li class="error" v-for="(error, index) in errors" :key="index">
-                    {{ error }}
-                </li>
-            </ul>
+                <ul v-if="errors">
+                    <li class="error" v-for="(error, index) in errors" :key="index">
+                        {{ error }}
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import { axios } from "../../common/app.js";
+import Validator from 'validatorjs'
+
 export default {
     data() {
         return {
-            // Form is prefilled for demonstration purposes; remove in final application
-            // jill@harvard.edu/asdfasdf is one of our seed users from e28api/seeds/user.json
             data: {
                 name: "",
                 email: "",
                 password: "",
             },
+            registerView: false,
             errors: null,
             favorites: [],
         };
@@ -86,6 +87,12 @@ export default {
         }
     },
     methods: {
+        switchToLoginView() {
+            this.registerView = false;
+        },
+        switchToRegisterView() {
+            this.registerView = true;
+        },
         login() {
             axios.post("login", this.data).then((response) => {
                 if (response.data.authenticated) {
@@ -121,6 +128,21 @@ export default {
                     this.$store.commit("setUser", null);
                 }
             });
+        },
+        validate() {
+            let validator = new Validator(this.data, {
+                name: "between:3,100",
+                email: "required|email",
+                password: "required"
+            });
+
+            if (validator.fails()) {
+                this.errors = validator.errors.all();
+            } else {
+                this.errors = null;
+            }
+
+            return validator.passes();
         },
     }
 };
--- p3/src/components/pages/RecipeCreatePage.vue
+++ p3/src/components/pages/RecipeCreatePage.vue
@@ -4,13 +4,13 @@
 
         <div id="inputs">
             <label for="name">Name</label>
-            <input type="text" v-model="recipe.name" id="name" />
+            <input type="text" v-model="recipe.name" id="name" v-on:blur="validate"/>
             <br />
             <label for="description">Description</label>
-            <textarea v-model="recipe.description" id="description"></textarea>
+            <textarea v-model="recipe.description" id="description" v-on:blur="validate"></textarea>
             <br />
             <label for="ingredients">Ingredients</label>
-            <input type="text" v-model="recipe.ingredients" id="ingredients" />
+            <input type="text" v-model="recipe.ingredients" id="ingredients" v-on:blur="validate"/>
         </div>
 
         <button v-on:click="addRecipe">Add Recipe</button>
@@ -23,6 +23,7 @@
 
 <script>
 import { axios } from "@/common/app.js";
+import Validator from 'validatorjs'
 
 export default {
     data() {
@@ -38,15 +39,32 @@ export default {
     },
     methods: {
         addRecipe() {
-            axios.post("/recipe", this.recipe).then((response) => {
-                if (response.data.errors) {
-                    this.errors = response.data.errors;
-                    this.success = false;
-                } else {
-                    this.$emit("update-recipes");
-                    this.success = true;
-                }
+            if (this.validate()) {
+                axios.post("/recipe", this.recipe).then((response) => {
+                    if (response.data.errors) {
+                        this.errors = response.data.errors;
+                        this.success = false;
+                    } else {
+                        this.$emit("update-recipes");
+                        this.success = true;
+                    }
+                });
+            }
+        },
+        validate() {
+            let validator = new Validator(this.recipe, {
+                name: "required|between:3,100",
+                description: "required|max:100",
+                ingredients: "required"
             });
+
+            if (validator.fails()) {
+                this.errors = validator.errors.all();
+            } else {
+                this.errors = null;
+            }
+
+            return validator.passes();
         },
     }
 };
